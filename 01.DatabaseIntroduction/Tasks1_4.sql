ed731c2580fee463bd3936aba0bf97d696cd30be
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-01-13 16:59:59 +0200

    Completed tasks 1 to 4.

---
+++ 01.DatabaseIntroduction/Tasks1_4.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE Minions;
+GO
+USE Minions;
+
+CREATE TABLE Minions
+(
+Id INT  PRIMARY KEY NOT NULL,
+[Name] NVARCHAR(50),
+Age TINYINT
+)
+
+CREATE TABLE Towns
+(
+Id INT PRIMARY KEY NOT NULL,
+[Name] VARCHAR(50)
+)
+
+ALTER TABLE Minions
+ADD TownId INT FOREIGN KEY REFERENCES Towns(Id)
+
+INSERT INTO Towns
+VALUES
+(1,'Sofia'),
+(2,'Plovdiv'),
+(3,'Varna')
+
+INSERT INTO Minions
+VALUES
+(1,'Kevin',22,1),
+(2,'Bob',15,3),
+(3,'Steward',NULL,2)
+
+
+
