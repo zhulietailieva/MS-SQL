c1ede5f827d38779c37cc05f4d192c5fde58e2e5
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-01-20 15:11:57 +0200

    Completed task 1.

---
+++ 03.TableRelations/Task1AlterTableAddPrimaryAndForeignKeys.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE Exercises
+GO
+USE Exercises
+
+CREATE TABLE Persons
+(
+PersonID INT NOT NULL,
+FirstName NVARCHAR(20) NOT NULL,
+Salary DECIMAL(8,2) NOT NULL,
+PassportID INT NOT NULL
+)
+
+INSERT INTO Persons
+VALUES
+(1,'Roberto',43300.00,102),
+(2,'Tom',56100.00,103),
+(3,'Yana',60200.00,101)
+
+CREATE TABLE Passports
+(
+PassportID INT NOT NULL,
+PassportNumber CHAR(8) NOT NULL
+)
+
+INSERT INTO Passports
+VALUES
+(101,'N34FG21B'),
+(102,'K65LO4R7'),
+(103,'ZE657QP2')
+
+SELECT * FROM Persons
+SELECT * FROM Passports
+
+ALTER TABLE Persons
+ADD CONSTRAINT PK_PersonID PRIMARY KEY(PersonID)
+
+ALTER TABLE Passports
+ADD CONSTRAINT PK_PassportID PRIMARY KEY(PassportID)
+
+ALTER TABLE Persons
+ADD CONSTRAINT FK_PassportID FOREIGN KEY(PassportID) REFERENCES Passports(PassportID)
+
