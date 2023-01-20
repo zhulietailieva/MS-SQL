818f5ad368e85a697004c8433219485e62811559
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-01-20 15:38:44 +0200

    Completed task 3.

---
+++ 03.TableRelations/Task3.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Students
+(
+StudentID INT NOT NULL,
+[Name] VARCHAR(50) NOT NULL
+)
+
+INSERT INTO Students
+VALUES
+(1,'Mila'),
+(2,'Toni'),
+(3,'Ron')
+
+CREATE TABLE Exams
+(
+ExamID INT NOT NULL,
+[Name] VARCHAR(20) NOT NULL
+)
+
+INSERT INTO Exams
+VALUES
+(101,'SpringMVC'),
+(102,'Neo4j'),
+(103,'Oracle 11g')
+
+SELECT * FROM Students
+SELECT * FROM Exams
+
+
+ALTER TABLE Students
+ADD CONSTRAINT PK_StudentID PRIMARY KEY(StudentID)
+
+ALTER TABLE Exams
+ADD CONSTRAINT PK_ExamID PRIMARY KEY(ExamID)
+
+CREATE TABLE StudentsExams
+(
+StudentID INT NOT NULL FOREIGN KEY REFERENCES Students(StudentID),
+ExamID INT NOT NULL FOREIGN KEY REFERENCES Exams(ExamID),
+PRIMARY KEY (StudentID,ExamID)
+)
+
+INSERT INTO StudentsExams
+VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103)
+
+SELECT * FROM StudentsExams
+
+
+
