20bce451d26cb8b1f4fbccb7fd2cd42de778f477
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-01-29 14:23:36 +0200

    Completed task 12.

---
+++ 05.SubqueriesAndJoins/Task12.sql
@@ -0,0 +1,12 @@
+
+SELECT c.CountryCode,m.MountainRange,p.PeakName,p.Elevation
+FROM Countries AS c
+JOIN MountainsCountries AS mc
+ON c.CountryCode=mc.CountryCode
+JOIN Mountains as m
+ON mc.MountainId=m.Id
+JOIN Peaks AS p
+ON m.Id=p.MountainId
+WHERE c.CountryCode='BG'
+AND p.Elevation>2835
+ORDER BY p.Elevation DESC
