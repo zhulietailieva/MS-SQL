49b7d4535fbb23ad814cb7980926cb61db654d95
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-01-29 14:40:44 +0200

    Fixed task 14.

--- 05.SubqueriesAndJoins/Task14.sql
+++ 05.SubqueriesAndJoins/Task14.sql
@@ -2,9 +2,9 @@
 SELECT TOP(5)
 c.CountryName,r.RiverName 
 FROM Countries AS c
-JOIN CountriesRivers AS cr
+LEFT JOIN CountriesRivers AS cr
 ON c.CountryCode=cr.CountryCode
-JOIN Rivers AS r
+LEFT JOIN Rivers AS r
 ON cr.RiverId=r.Id
 WHERE ContinentCode='AF'
 ORDER BY c.CountryName
