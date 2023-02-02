df1bd76b3039fdc543a5ed855c31f1d53c7d2f19
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-02-02 22:53:03 +0200

    Made small changes.

--- 06.IndicesAndDataAggregation/Task15CopyDataFromOneTableToANewOne.sql
+++ 06.IndicesAndDataAggregation/Task15CopyDataFromOneTableToANewOne.sql
@@ -2,16 +2,16 @@ SELECT * INTO [AverageSalaries]
 	FROM [Employees]
    WHERE [Salary] > 30000
 
-DELETE FROM AverageSalaries
-WHERE ManagerID=42
+DELETE FROM [AverageSalaries]
+WHERE [ManagerID]=42
 
-SELECT * FROM AverageSalaries
+--SELECT * FROM [AverageSalaries]
 
-UPDATE AverageSalaries
-SET Salary=Salary+5000
-WHERE DepartmentId=1
+UPDATE [AverageSalaries]
+	SET [Salary]+=5000
+	WHERE [DepartmentID]=1
 
-SELECT DepartmentID,AVG(Salary) AS [AverageSalary]
-FROM AverageSalaries
-GROUP BY DepartmentID
+SELECT [DepartmentID], AVG([Salary]) AS [AverageSalary]
+FROM [AverageSalaries]
+GROUP BY [DepartmentID]
 	
